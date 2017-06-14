1f8fbd26daad57d5987511e576a0886a4889f940
Carl Page <carlpage@Carls-MacBook-Air.local>
2017-06-14 14:23:57 -0500

    finished assignment

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -29,7 +29,7 @@ myApp.controller('HttpController', function($http) {
       url: 'http://api.giphy.com/v1/gifs/search?q=' + vm.search + '&api_key=dc6zaTOxFJmzC'
     }).then(function(response) {
       console.log('back with: ', response);
-      vm.search = response.data.data;
+      vm.imgResp = response.data.data;
     });
   };
 }); // end HttpController
