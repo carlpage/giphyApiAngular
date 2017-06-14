e1c780bf7f0bf3bf2e6a11f0b21e3cf68fbb33bf
Carl Page <carlpage@Carls-MacBook-Air.local>
2017-06-14 13:52:22 -0500

    almost there

---
+++ public/scripts/script.js
@@ -0,0 +1,35 @@
+// declare our intentions to use an Angular app
+var myApp = angular.module('myApp', []);
+
+myApp.controller('HttpController', function($http) {
+  console.log('NG');
+  var vm = this;
+// temp start up function
+  vm.startUpFunction = function() {
+    console.log('I am here in startUpFunction');
+
+  }; // end startUpFunction
+
+  vm.randomGiphy = function() {
+    console.log('Got a giphy');
+
+    $http({
+      method: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='
+    }).then(function(response) {
+      console.log('yuuuuupp: ', response);
+      vm.rando = response.data.data;
+      console.log('rando is: ', vm.rando);
+    });
+  };
+  
+  vm.searchGiphy = function() {
+    console.log('Got a giphy');
+    $http({
+      method: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/search?q=' + vm.search + '&api_key=dc6zaTOxFJmzC'
+    }).then(function(response) {
+      console.log('back with: ', response);
+    });
+  };
+}); // end HttpController
